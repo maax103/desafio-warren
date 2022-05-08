64f6d02e63bd2e68af875f37127fe30311ac2b83
Max Furtado <maximilianomfurtado@gmail.com>
2022-05-08 16:27:43 -0300

    melhorado landing page

--- src/components/Page/Intro/index.js
+++ src/components/Page/Intro/index.js
@@ -5,6 +5,10 @@ export const Intro = () => {
     return (
         <IntroContainer>
             <h1>DESAFIO WARREN - 2022</h1>
+            <p>Este repositório foi criado como resolução ao Desafio Warren do programa de formação e recrutamento da ProWay em parceria com a Warren Academy.</p>
+            <p><a href="https://github.com/maax103/desafio-warren">
+                    Clique aqui
+                </a> para ver os detalhes deste projeto </p>
         </IntroContainer>
     )
 }
--- src/components/Page/Intro/style.js
+++ src/components/Page/Intro/style.js
@@ -1,5 +1,20 @@
 import styled from "styled-components";
 
 export const IntroContainer = styled.div`
+    width: 45vw;
+    /* border: 1px solid red; */
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
 
+    a, a:visited{
+        color: white;
+        text-decoration: none;
+        font-weight: bold;
+    }
+    p{
+        margin-top: 2rem;
+        text-align: center;
+    }
 `
